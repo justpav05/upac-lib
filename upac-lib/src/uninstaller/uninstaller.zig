2d53b6040a4ba3eaec798cf5e53d6922f1450586
JustPav <aksenovpaveldmitrievich@gmail.com>
2026-04-06 22:02:00 +0400

    NEW: File deletion functionality has been completely overhauled and now relies on a new engine. BREAKING CHANGE: API behavior has been modified and file structures completely redesigned; the API is no longer compatible.

--- upac-lib/src/uninstaller/uninstaller.zig
+++ upac-lib/src/uninstaller/uninstaller.zig
@@ -1,25 +1,57 @@
 const std = @import("std");
-const posix = std.posix;
 
 const database = @import("upac-database");
+const PackageMeta = database.PackageMeta;
 
-const fsm = @import("fsm.zig");
+const file = @import("upac-file");
+const c_libs = file.c_libs;
+
+const states = @import("states.zig");
+
+pub const UninstallerError = error{
+    PackageNotFound,
+    RepoPathNotFound,
+    RepoOpenFailed,
+    MaxRetriesExceeded,
+};
 
 pub const StateId = enum {
-    reading_files,
-    removing_links,
-    removing_files,
-    unregistering,
+    verifying,
+    open_repo,
+    check_installed,
+    load_files,
+    remove_files,
+    remove_db_files,
+    commit,
+
     done,
     failed,
 };
 
+pub const UninstallData = struct {
+    package_name: []const u8,
+
+    repo_path: []const u8,
+    database_path: []const u8,
+    checkout_path: []const u8,
+
+    branch_name: []const u8,
+
+    max_retries: u8 = 0,
+};
+
 pub const UninstallerMachine = struct {
     data: UninstallData,
-    stack: std.ArrayList(StateId),
     retries: u8,
+
+    repo: ?*c_libs.OstreeRepo,
+    mtree: ?*c_libs.OstreeMutableTree,
+
+    package_checksum: ?[]const u8,
+    package_file_map: ?database.FileMap,
+
+    stack: std.ArrayList(StateId),
     allocator: std.mem.Allocator,
-    files: ?[][]const u8,
 
     pub fn enter(self: *UninstallerMachine, state_id: StateId) !void {
         try self.stack.append(state_id);
@@ -34,31 +66,31 @@ pub const UninstallerMachine = struct {
     }
 
     pub fn deinit(self: *UninstallerMachine) void {
+        if (self.pkg_checksum) |checksum| self.allocator.free(checksum);
+        if (self.pkg_file_map) |*file_map| database.freeFileMap(file_map, self.allocator);
+
+        if (self.mtree) |mtree| c_libs.g_object_unref(mtree);
+        if (self.repo) |repo| c_libs.g_object_unref(repo);
+
         self.stack.deinit();
-        if (self.files) |files| {
-            for (files) |file_path| self.allocator.free(file_path);
-            self.allocator.free(files);
-        }
+    }
+
+    pub fn run(uninstall_data: UninstallData, allocator: std.mem.Allocator) !void {
+        var machine = UninstallerMachine{
+            .data = uninstall_data,
+            .retries = 0,
+
+            .repo = null,
+            .mtree = null,
+
+            .pkg_checksum = null,
+            .pkg_file_map = null,
+
+            .stack = std.ArrayList(StateId).init(allocator),
+            .allocator = allocator,
+        };
+        defer machine.deinit();
+
+        try states.stateVerifying(&machine);
     }
 };
-
-pub const UninstallData = struct {
-    package_name: []const u8,
-    root_path: []const u8,
-    repo_path: []const u8,
-    database_path: []const u8,
-    max_retries: u8 = 0,
-};
-
-pub fn uninstall(uninstall_data: UninstallData, allocator: std.mem.Allocator) !void {
-    var machine = UninstallerMachine{
-        .data = uninstall_data,
-        .stack = std.ArrayList(StateId).init(allocator),
-        .retries = 0,
-        .allocator = allocator,
-        .files = null,
-    };
-    defer machine.deinit();
-
-    try fsm.stateReadingFiles(&machine);
-}
